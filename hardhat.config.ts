5e4bf98e2ddff197a957ee91e7b539a099c5e103
mikekwanai <JoganicGardener23041979tvmk@hotmail.com>
2025-12-25 14:56:44 +0800

    Hardhat config: switch to EncryptoVault

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,16 +6,22 @@ import "@typechain/hardhat";
 import "hardhat-deploy";
 import "hardhat-gas-reporter";
 import type { HardhatUserConfig } from "hardhat/config";
-import { vars } from "hardhat/config";
 import "solidity-coverage";
 
 import "./tasks/accounts";
-import "./tasks/FHECounter";
+import "./tasks/encryptoVault";
 
-// Run 'npx hardhat vars setup' to see the list of variables that need to be set
+import * as dotenv from "dotenv";
+dotenv.config();
 
-const MNEMONIC: string = vars.get("MNEMONIC", "test test test test test test test test test test test junk");
-const INFURA_API_KEY: string = vars.get("INFURA_API_KEY", "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz");
+const privateKey = process.env.PRIVATE_KEY;
+const parsedPrivateKey = privateKey
+  ? privateKey.startsWith("0x")
+    ? privateKey
+    : `0x${privateKey}`
+  : undefined;
+const sepoliaAccounts = parsedPrivateKey ? [parsedPrivateKey] : [];
+const INFURA_API_KEY: string = process.env.INFURA_API_KEY ?? "";
 
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
@@ -24,7 +30,7 @@ const config: HardhatUserConfig = {
   },
   etherscan: {
     apiKey: {
-      sepolia: vars.get("ETHERSCAN_API_KEY", ""),
+      sepolia: process.env.ETHERSCAN_API_KEY ?? "",
     },
   },
   gasReporter: {
@@ -34,28 +40,17 @@ const config: HardhatUserConfig = {
   },
   networks: {
     hardhat: {
-      accounts: {
-        mnemonic: MNEMONIC,
-      },
       chainId: 31337,
     },
     anvil: {
-      accounts: {
-        mnemonic: MNEMONIC,
-        path: "m/44'/60'/0'/0/",
-        count: 10,
-      },
       chainId: 31337,
       url: "http://localhost:8545",
+      accounts: sepoliaAccounts.length > 0 ? sepoliaAccounts : undefined,
     },
     sepolia: {
-      accounts: {
-        mnemonic: MNEMONIC,
-        path: "m/44'/60'/0'/0/",
-        count: 10,
-      },
       chainId: 11155111,
-      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      url: INFURA_API_KEY ? `https://sepolia.infura.io/v3/${INFURA_API_KEY}` : "",
+      accounts: sepoliaAccounts,
     },
   },
   paths: {
